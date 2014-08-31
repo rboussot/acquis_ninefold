79fa0ffa0ec71d25dbdff7187b2671056580ed2a
Romain Boussot <rboussot@gmail.com>
2014-08-31 08:11:23 +0200

    enlever la colonne video de la table Passages

--- db/migrate/20140722095902_create_passages.rb
+++ db/migrate/20140722095902_create_passages.rb
@@ -3,7 +3,6 @@ class CreatePassages < ActiveRecord::Migration
     create_table :passages do |t|
       t.integer :oeuvre_id
       t.string :titre_passage
-      t.string :video
 
       t.timestamps
     end
