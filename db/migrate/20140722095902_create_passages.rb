78dcd83e2884b6aebe9282b81e4c80b5bfd1f4f8
Romain Boussot <rboussot@gmail.com>
2014-08-31 08:06:04 +0200

    correction du champ video dans la table passages

--- db/migrate/20140722095902_create_passages.rb
+++ db/migrate/20140722095902_create_passages.rb
@@ -3,7 +3,7 @@ class CreatePassages < ActiveRecord::Migration
     create_table :passages do |t|
       t.integer :oeuvre_id
       t.string :titre_passage
-      t.video :string
+      t.string :video
 
       t.timestamps
     end
